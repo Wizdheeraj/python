4d065a7f8a2a58a23cc74cb93c4a333632350812
Wizdheeraj <156817070+Wizdheeraj@users.noreply.github.com>
2024-01-16 20:25:24 +0530

    Add files via upload

---
+++ prg1.py
@@ -0,0 +1,10 @@
+a=10
+b="Python"
+c = 10.5
+d=2.14j
+e=True
+print("Data type of Variable a :",type(a))
+print("Data type of Variable b :",type(b))
+print("Data type of Variable c :",type(c))
+print("Data type of Variable d :",type(d))
+print("Data type of Variable e :",type(e))
